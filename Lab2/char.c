9b1c3da6d8ae1570214124e550dde1123ed1755d
Muhd Hilmi <2020853968@student.uitm.edu.my>
2022-06-25 02:11:39 +0800

    initial commit

---
+++ Lab2/char.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void){
+	int pipefds[2];
+	char buffer[5];
+
+	if(pipe(pipefds) == -1)
+	{
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+
+	char *pin = "4128\0";
+
+	printf("writing PIN to pipe...\n");
+	write(pipefds[1], pin, 5);
+	printf("Done. \n\n");
+
+	printf("PIN from pipe: %s\n", buffer);
+
+	return EXIT_SUCCESS;
+}
